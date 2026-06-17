b96dd0a179ea3a43df09b2ce1041f3fb932917e6
NguyenNhatHuy-se204410 <nhathuyhs776@gmail.com>
2026-06-17 09:06:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-function upDate(previewPic){
+function upDate(previewPic) {
+    console.log("Image event triggered");
 
-    document.getElementById("image").innerHTML =
-        previewPic.alt;
+    document.getElementById("image").innerHTML = previewPic.alt;
 
     document.getElementById("image").style.backgroundImage =
         "url('" + previewPic.src + "')";
 }
 
-function unDo(){
+function unDo() {
+    console.log("Image reset triggered");
 
-    document.getElementById("image").style.backgroundImage =
-        "url('')";
+    document.getElementById("image").style.backgroundImage = "url('')";
 
     document.getElementById("image").innerHTML =
-        "Hover over an image below to display here.";
+        "Hover over or focus on an image below to display here.";
+}
+
+function addTabFocus() {
+    console.log("Page loaded. Adding tabindex.");
+
+    let images = document.querySelectorAll(".grid img");
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].setAttribute("tabindex", "0");
+        console.log("tabindex added to image " + i);
+    }
 }
